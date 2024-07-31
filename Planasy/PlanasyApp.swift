9b715ccc8329f345c26ccb03e997d062f7b7c271
chef-nic <nicholasajohnson@me.com>
2024-07-31 15:59:52 -0700

    Initial Commit

---
+++ Planasy/PlanasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanasyApp.swift
+//  Planasy
+//
+//  Created by Nicholas Johnson on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
